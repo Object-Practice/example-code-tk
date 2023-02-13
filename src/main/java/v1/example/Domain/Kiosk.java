043f790ee5f40bd96b5eb93183bcd642fee594c5
constwon111 <constwon111@gmail.com>
2023-02-13 12:35:04 +0900

    [Modifieid] 가격 계산하는 로직과 card일경우와 현금일 경우에 kiosk에 각각 각각 card와 현금에 돈을 증가시키도록 수정

--- src/main/java/v1/example/Domain/Kiosk.java
+++ src/main/java/v1/example/Domain/Kiosk.java
@@ -1,5 +1,7 @@
 package v1.example.Domain;
 
+
 public interface Kiosk {
 	boolean sell(Product product, Customer customer, int amount);
+	Integer calculateProduct(Product product, Integer amount);
 }
--- src/main/java/v1/example/Infra/KioskImpl.java
+++ src/main/java/v1/example/Infra/KioskImpl.java
@@ -4,9 +4,15 @@ import v1.example.Domain.Kiosk;
 import v1.example.Domain.Product;
 import v1.example.Domain.StockManager;
 import v1.example.Domain.Customer;
+import v1.example.Infra.PayType.Card;
+import v1.example.Infra.PayType.Cash;
+
+import java.util.List;
 
 public class KioskImpl implements Kiosk {
 	private final StockManager stockManager;
+	Card card = new Card(0);
+	Cash cash = new Cash(0);
 
 	public KioskImpl(StockManager stockManager) {
 		this.stockManager = stockManager;
@@ -17,13 +23,27 @@ public class KioskImpl implements Kiosk {
 		if (stockManager.hasStock(product, amount) && product.isAvailableBy(customer)) {
 			customer.buy(product, amount);
 			stockManager.minusStock(product, amount);
+
+			Integer totalPrice = calculateProduct(product,amount);
+			if(customer.isCard()){
+				card.plusMoney(totalPrice);
+			}else{
+				cash.plusMoney(totalPrice);
+			}
 			return true;
 		} else {
 			return false;
 		}
 	}
 
+	@Override
+	public Integer calculateProduct(Product product, Integer amount) {
+		return product.getPrice() * amount;
+	}
+
+
 	public void showInfo(){
 		stockManager.showInfo();
 	}
+
 }
